6cf30b50ab546d4b88b54181c71cc75481b73bcf
cosmicfate <dylanronquille@gmail.com>
2019-12-10 23:08:54 +0000

    runtime

--- projects/runtime/js/level01.js
+++ projects/runtime/js/level01.js
@@ -6,14 +6,14 @@ var level01 = function (window) {
     var createjs = window.createjs;
 
     window.opspark.runLevelInGame = function(game) {
-        // some useful constants 
+        // some useful constants
         var groundY = game.groundY;
 
         // this data will allow us to define all of the
         // behavior of our game
         var levelData = {
-            name: "Robot Romp",
-            number: 1, 
+            name: "Zalinky",
+            number: 1,
             speed: -3,
             gameItems: [
                 {type: 'sawblade',x:400,y:groundY},
@@ -23,12 +23,28 @@ var level01 = function (window) {
         };
         window.levelData = levelData;
         // set this to true or false depending on if you want to see hitzones
-        game.setDebugMode(true);
+        game.setDebugMode(false);
 
         // BEGIN EDITING YOUR CODE HERE
 
+        function createSawBlade(xLocation, yLocation) {
+            var hitZoneSize = 25;
+            var damageFromObstacle = 10;
+            var myObstacle = game.createObstacle(hitZoneSize,damageFromObstacle);
+            myObstacle.x = xLocation;
+            myObstacle.y = yLocation;
+            game.addGameItem(myObstacle);
 
-    }
+            var obstacleImage = draw.bitmap('img/sawblade.png');
+            myObstacle.addChild(obstacleImage);
+
+            obstacleImage.x = -25;
+            obstacleImage.y = -25;
+        }
+
+        createSawBlade(400, groundY);
+
+    };
 };
 
 // DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
--- projects/runtime/js/view/background.js
+++ projects/runtime/js/view/background.js
@@ -1,10 +1,10 @@
 var background = function (window) {
     'use strict';
-    
+
     window.opspark = window.opspark || {};
     var draw = window.opspark.draw;
     var createjs = window.createjs;
-    
+
     /*
      * Create a background view for our game application
      */
@@ -16,18 +16,19 @@ var background = function (window) {
         if(!ground || typeof(ground.y) == 'undefined') {
             throw new Error("Invalid ground argument");
         }
-        
+
         // useful variables
         var canvasWidth = app.canvas.width;
         var canvasHeight = app.canvas.height;
         var groundY = ground.y;
-        
+
         // container which will be returned
         var background;
-        
+
         // ANIMATION VARIABLES HERE:
         var tree;
-     
+        var buildings = [];
+
         // called at the start of game and whenever the page is resized
         // add objects for display in background. draws each image added to the background once
         function render() {
@@ -38,36 +39,43 @@ var background = function (window) {
             // you should modify this to suit your game
             var backgroundFill = draw.rect(canvasWidth,groundY, '#b38349');
             background.addChild(backgroundFill);
-            
+
             // TODO: 3 - Add a moon and starfield
             var moon = draw.bitmap('img/beans.png');
             moon.x = 300;
             moon.y = groundY - 350;
             moon.scaleX = .4;
             moon.scaleY = .4;
-            
+
             var circle;
-            for(var i=0;i<100;i++) {
-                circle = draw.circle(4,'white','LightGray',2);
+            for(var i=0;i<100 ;i++) {
+                circle = draw.circle(2,'black','LightGray',2);
                 circle.x = canvasWidth*Math.random();
                 circle.y = groundY*Math.random();
                 background.addChild(circle);
             }
-            
+
             background.addChild(moon);
-            
+
             // TODO: 5 - Add buildings!     Q: This is before TODO 4 for a reason! Why?
-            
-            
+            for(var i=0;i<10;++i) {
+                var buildingHeight = Math.ceil(Math.random() * 250);
+                var building = draw.rect(75,buildingHeight,'LightGray','Black',1);
+                building.x = 200*i;
+                building.y = groundY-buildingHeight;
+                background.addChild(building);
+                buildings.push(building);
+            }
+
             // TODO 4: Part 1 - Add a tree
-            
+
             tree = draw.bitmap('img/CanofBeans.png');
             tree.x = 1000;
             tree.y = 1;
             background.addChild(tree);
         } // end of render function - DO NOT DELETE
-        
-        
+
+
         // Perform background animation
         // called on each timer "tick" - 60 times per second
         function update() {
@@ -75,29 +83,37 @@ var background = function (window) {
             var canvasWidth = app.canvas.width;
             var canvasHeight = app.canvas.height;
             var groundY = ground.y;
-            
+
             // TODO 4: Part 2 - Move the tree!
-            tree.x = tree.x - 1;
-            if(tree.x < -200) {
-                tree.x = canvasWidth;
-            }
-            
+            moveObject(tree, 1);
+
             // TODO 5: Part 2 - Parallax
-            
+
+            for(var i = 0; i < buildings.length; i++) {
+                var eachBuilding = buildings[i];
+                moveObject(eachBuilding, 0.5)
+            }
+
+            function moveObject(object, speed) {
+                object.x = object.x - speed;
+                if(object.x < -200) {
+                    object.x = canvasWidth;
+                }
+            }
 
         } // end of update function - DO NOT DELETE
-        
-        
-        
+
+
+
         /* Make a createjs Container for the background and let it know about the render and upate functions*/
         background = new createjs.Container();
         background.resize = render;
         background.update = update;
-        
+
         /* make the background able to respond to resizing and timer updates*/
         app.addResizeable(background);
         app.addUpdateable(background);
-        
+
         /* render and return the background */
         render();
         return background;
