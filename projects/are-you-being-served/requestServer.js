ad011949a74b8822b0b761a6c93d04a791da3ae4
dylanronquille <git-commit-address>
2021-03-18 10:18:44 -0500

    push

---
+++ projects/are-you-being-served/requestServer.js
@@ -0,0 +1,28 @@
+// requestServer.js file
+const http = require('http');
+const request = require('request')
+const args = process.argv.slice(2);
+var port = 8686;
+
+const server = http.createServer(function(req,res){
+var url = args[0] ? args[0] : "<a default url>"
+
+
+
+request(url, function(err, response, body){
+    if(!err && response.statusCode === 200){
+        res.writeHead(200,{'Content-type': ' text/html'})
+        res.write(body)
+        console.log("running");
+        res.end();
+    } else {
+        res.writeHead(response.statusCode,{'Content-type': 'text/plain'})
+        res.write(err.toString());
+        
+    }
+    res.end ();
+})
+
+});
+
+server.listen(port);
