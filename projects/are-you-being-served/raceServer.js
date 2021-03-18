c98f257a3e498061c05bba1147b4c52dd5854e9c
dylanronquille <git-commit-address>
2021-03-18 10:36:03 -0500

    swapping test-guru

--- projects/are-you-being-served/raceServer.js
+++ projects/are-you-being-served/raceServer.js
@@ -16,11 +16,32 @@ let startTime = d.getTime();
     async.series( 
         // TODO 8: Supply an array of functions
         [
-            
+            function one (callback){
+                wrapper(callback)
+            },
+            function two (callback){
+                wrapper(callback)
+            },
+            function three (callback){
+                wrapper(callback)
+            },
+            function four (callback){
+                wrapper(callback)
+            }
+
         ],
         function (error, results) {
             // TODO 9: add a callback function to the end of the async call to tally the results 
-
+                res.write("Results:\n")
+                var victoryOrder = sortTogether(racers,results);
+                for( var i = 0; i < victoryOrder.length; i++){
+                    res.write(victoryOrder[i])
+                    res.write("\n")
+                }
+                let dt  = new Date();
+                let endTime = dt.getTime();
+                let raceTime = enditme - startTime;
+                res.write("raceTime");
         }
     );
     
@@ -28,15 +49,18 @@ let startTime = d.getTime();
 
 // TODO 7: create a common function to be called by all functions in the array passed to the async function
 function wrapper(callback){
-
-}
+    setTimeout(function()
+{
+    var d = new Date();
+    callback(null, d.getTime());
+}, Math.random()*1000);
 
 function sortTogether(names, times) {
     var tempList = [];
     for (var i = 0; i < names.length; i++) {
         tempList.push({'name': names[i], 'time': times[i]});
     }
-
+}
     tempList.sort(function(a, b) {
         return ((a.time < b.time) ? -1 : ((a.time == b.time) ? 0 : 1));
     });
