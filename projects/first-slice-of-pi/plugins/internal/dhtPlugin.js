cf67c55f219ca78125402807ca4579118329dd41
cosmicfate <dylanronquille@gmail.com>
2021-04-22 18:23:50 -0500

    adding pirPlugin start and dhtPlugin

---
+++ projects/first-slice-of-pi/plugins/internal/dhtPlugin.js
@@ -0,0 +1,40 @@
+var resources = require('./../../resources/model');
+var sensorDriver = require('node-dht-sensor');
+
+function connectHardware(){
+    var sensor = {
+        initialize: function (){
+            sensorDriver.initialize(device.model, device.gpio)
+            
+        },
+        read: function (){data = sensorDriver.read(); 
+            device.temperature.value = parseFloat(data.temperature); 
+            device.humidity.value = parseFloat(data.humidity);
+       
+
+        }
+    };
+
+    sensor.initialize();
+    sensor.read();
+
+    interval = setInterval(function () {
+		sensor.read();
+	}, localParams.frequency);
+};
+
+function start(params){
+    localParams = params ? params : localParams;
+    connectHardware();
+    
+};
+
+function stop(){
+    clearInterval(interval)
+};
+
+var interval, sensor;
+var device = resources.pi.sensors.dht;
+var localParams = {'frequency': 2000};
+
+
---
+++ projects/first-slice-of-pi/wot-server.js
@@ -0,0 +1,17 @@
+var httpServer = require('./servers/http'),
+	resources = require('./resources/model');
+
+var pirPlugin = require('./plugins/internal/pirPlugin');
+
+pirPlugin.start({});
+dhtPlugin.start({'frequency': 2000});
+
+var server = httpServer.listen(resources.pi.port, function () {
+	console.log("Running the Pi on port " + resources.pi.port);
+});
+
+process.on('SIGINT', function() {
+	pirPlugin.stop();
+	dhtPlugin.stop();
+	process.exit();
+});
